75453c1a9d00c4f7c0efb8f8e39526dcf3a98ba2
Alex Solomaha <cyanofresh@gmail.com>
2017-06-22 10:50:19 +0300

    Fixed fonts

--- assets/FontsAsset.php
+++ assets/FontsAsset.php
@@ -7,7 +7,6 @@ use yii\web\AssetBundle;
 class FontsAsset extends AssetBundle
 {
     public $css = [
-        'https://fonts.googleapis.com/css?family=Product+Sans',
-        'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
+        'https://fonts.googleapis.com/css?family=Product+Sans|Roboto:300,400,500,700',
     ];
 }
